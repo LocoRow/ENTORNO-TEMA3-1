3aa5b34a72895a3100d730ce02be7da4ad4095e6
PEDRO PUERTA VAZQUEZ <pedpuevaz@alu.edu.gva.es>
2022-11-22 11:46:16 +0100

    Añadido el código del proyecto

--- Ejercicio1 Pedro-Jorge/Form1.Designer.cs
+++ Ejercicio1 Pedro-Jorge/Form1.Designer.cs
@@ -2,46 +2,120 @@
 {
     partial class Form1
     {
-        /// <summary>
-        /// Variable del diseñador necesaria.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
+            /// <summary>
+            /// Variable del diseñador necesaria.
+            /// </summary>
+            private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Limpiar los recursos que se estén usando.
-        /// </summary>
-        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null))
+            /// <summary>
+            /// Limpiar los recursos que se estén usando.
+            /// </summary>
+            /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+            protected override void Dispose(bool disposing)
             {
-                components.Dispose();
+                if (disposing && (components != null))
+                {
+                    components.Dispose();
+                }
+                base.Dispose(disposing);
             }
-            base.Dispose(disposing);
+
+            #region Código generado por el Diseñador de Windows Forms
+
+            /// <summary>
+            /// Método necesario para admitir el Diseñador. No se puede modificar
+            /// el contenido de este método con el editor de código.
+            /// </summary>
+            private void InitializeComponent()
+            {
+                this.txtPrecio = new System.Windows.Forms.TextBox();
+                this.lblCoste = new System.Windows.Forms.Label();
+                this.btnCalcular = new System.Windows.Forms.Button();
+                this.cbUrgente = new System.Windows.Forms.CheckBox();
+                this.txtTelegrama = new System.Windows.Forms.RichTextBox();
+                this.lblTexto = new System.Windows.Forms.Label();
+                this.SuspendLayout();
+                // 
+                // txtPrecio
+                // 
+                this.txtPrecio.Location = new System.Drawing.Point(250, 287);
+                this.txtPrecio.Name = "txtPrecio";
+                this.txtPrecio.Size = new System.Drawing.Size(100, 20);
+                this.txtPrecio.TabIndex = 0;
+                // 
+                // lblCoste
+                // 
+                this.lblCoste.AutoSize = true;
+                this.lblCoste.Location = new System.Drawing.Point(198, 290);
+                this.lblCoste.Name = "lblCoste";
+                this.lblCoste.Size = new System.Drawing.Size(37, 13);
+                this.lblCoste.TabIndex = 1;
+                this.lblCoste.Text = "Coste:";
+                // 
+                // btnCalcular
+                // 
+                this.btnCalcular.Location = new System.Drawing.Point(454, 280);
+                this.btnCalcular.Name = "btnCalcular";
+                this.btnCalcular.Size = new System.Drawing.Size(101, 27);
+                this.btnCalcular.TabIndex = 2;
+                this.btnCalcular.Text = "Calcular";
+                this.btnCalcular.UseVisualStyleBackColor = true;
+                this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+                // 
+                // cbUrgente
+                // 
+                this.cbUrgente.AutoSize = true;
+                this.cbUrgente.Location = new System.Drawing.Point(201, 245);
+                this.cbUrgente.Name = "cbUrgente";
+                this.cbUrgente.Size = new System.Drawing.Size(70, 17);
+                this.cbUrgente.TabIndex = 3;
+                this.cbUrgente.Text = "Urgente?";
+                this.cbUrgente.UseVisualStyleBackColor = true;
+                // 
+                // txtTelegrama
+                // 
+                this.txtTelegrama.Location = new System.Drawing.Point(201, 124);
+                this.txtTelegrama.Name = "txtTelegrama";
+                this.txtTelegrama.Size = new System.Drawing.Size(354, 96);
+                this.txtTelegrama.TabIndex = 4;
+                this.txtTelegrama.Text = "";
+                // 
+                // lblTexto
+                // 
+                this.lblTexto.AutoSize = true;
+                this.lblTexto.Location = new System.Drawing.Point(209, 108);
+                this.lblTexto.Name = "lblTexto";
+                this.lblTexto.Size = new System.Drawing.Size(37, 13);
+                this.lblTexto.TabIndex = 5;
+                this.lblTexto.Text = "Texto:";
+                // 
+                // Form1
+                // 
+                this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+                this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+                this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+                this.ClientSize = new System.Drawing.Size(800, 450);
+                this.Controls.Add(this.lblTexto);
+                this.Controls.Add(this.txtTelegrama);
+                this.Controls.Add(this.cbUrgente);
+                this.Controls.Add(this.btnCalcular);
+                this.Controls.Add(this.lblCoste);
+                this.Controls.Add(this.txtPrecio);
+                this.Name = "Form1";
+                this.Text = "Form1";
+                this.ResumeLayout(false);
+                this.PerformLayout();
+
+            }
+
+            #endregion
+
+            private System.Windows.Forms.TextBox txtPrecio;
+            private System.Windows.Forms.Label lblCoste;
+            private System.Windows.Forms.Button btnCalcular;
+            private System.Windows.Forms.CheckBox cbUrgente;
+            private System.Windows.Forms.RichTextBox txtTelegrama;
+            private System.Windows.Forms.Label lblTexto;
         }
-
-        #region Código generado por el Diseñador de Windows Forms
-
-        /// <summary>
-        /// Método necesario para admitir el Diseñador. No se puede modificar
-        /// el contenido de este método con el editor de código.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            this.SuspendLayout();
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(569, 339);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.ResumeLayout(false);
-
-        }
-
-        #endregion
     }
-}
 
--- Ejercicio1 Pedro-Jorge/Form1.cs
+++ Ejercicio1 Pedro-Jorge/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,5 +17,59 @@ namespace Ejercicio1_Pedro_Jorge
         {
             InitializeComponent();
         }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            string textoTelegrama;
+            char tipoTelegrama = 'o';
+            int numPalabras = 0;
+            double coste;
+
+            int cuentaPalabras = 0, i = 0; 
+
+            textoTelegrama = txtTelegrama.Text;
+
+            if (cbUrgente.Checked)
+                tipoTelegrama = 'u';
+
+            //Erróneo numPalabras = textoTelegrama.Length;
+
+            // Cálculo del número de palabras de una cadena 
+            /*for (i=0;i< textoTelegrama.Length;i++)
+            {
+                if (textoTelegrama[i].ToString() == " ")
+                    cuentaPalabras ++; 
+
+            }*/
+
+            while (textoTelegrama.IndexOf(" ") != -1)
+            {
+                cuentaPalabras++;
+                textoTelegrama = textoTelegrama.Substring(textoTelegrama.IndexOf(" ") + 1, textoTelegrama.Length - textoTelegrama.IndexOf(" ") - 1);
+            }
+
+
+
+            if (textoTelegrama.Length > 0)
+                cuentaPalabras++;
+
+            if (tipoTelegrama != 'u')
+                if (numPalabras <= 10)
+                    coste = 2.5;
+                else
+                    coste = 0.5 * numPalabras;
+            else
+            //Si el telegrama es urgente
+            if (tipoTelegrama == 'u')
+                if (numPalabras <= 10)
+                    coste = 5;
+                else
+                    coste = 5 + 0.75 * (numPalabras - 10);
+            else
+                coste = 0;
+            txtPrecio.Text = coste.ToString() + " euros";
+        }
+
     }
 }
+
