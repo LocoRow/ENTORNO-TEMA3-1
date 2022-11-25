3b78165c8c4765b28c3596087c2dd55274346ff9
JorgeP <jpastor4789@gmail.com>
2022-11-25 11:32:56 +0100

    comentario añadido a rama comentario

--- Ejercicio1 Pedro-Jorge/Form1.cs
+++ Ejercicio1 Pedro-Jorge/Form1.cs
@@ -68,7 +68,9 @@ namespace Ejercicio1_Pedro_Jorge
             else
                 coste = 0;
             txtPrecio.Text = coste.ToString() + " euros ";
-            /* Pedro es un pringao*/
+            /* Pedro es un pringao*
+             * 
+             * añado comentario en la rama comentario/
         }
 
     }
